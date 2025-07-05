cb192bba56a3612847d1f3ebe2b7b774bb71645c
LeHuyTuong <lehuytuong2005@gmail.com>
2025-07-05 17:43:55 +0700

    Add update and login with 19p

---
+++ PE_SU23_200912/src/java/pe/model/tblUsersDAO.java
@@ -0,0 +1,56 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.model;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import pe.utils.DBHepler;
+
+/**
+ *
+ * @author USER
+ */
+public class tblUsersDAO implements Serializable{
+    public tblUsersDTO checkLogin(String userID, String password)
+    throws SQLException, ClassNotFoundException{
+        tblUsersDTO result = null;
+        Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try{
+            con = DBHepler.getConnection();
+            if(con != null){
+                String query = "Select fullName, roleID "
+                        + "From tblUsers "
+                        + "Where userID = ? "
+                        + "And password = ? ";
+                stm = con.prepareStatement(query);
+                stm.setString(1, userID);
+                stm.setString(2, password);
+                rs = stm.executeQuery();
+                if(rs.next()){
+                    String fullName = rs.getString("fullName");
+                    String role = rs.getString("roleID");
+                    result = new tblUsersDTO(userID, fullName, role, password);
+                }
+            }
+        }finally{
+            if(rs != null){
+                rs.close();
+            }
+            if(stm != null){
+                stm.close();
+            }
+            if(con != null){
+                con.close();
+            }
+        }
+        
+        return result;
+    }
+}
